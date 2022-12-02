1df4ed8299365895de160e2b2a0466485805c981
xeno <xeno@lv427.space>
2022-12-02 23:52:12 +0100

    some optimizations

--- day2.c
+++ day2.c
@@ -7,10 +7,10 @@ int main() {
     int score1 = 0;
     int score2 = 0;
     while (fscanf(input,"%c %c\n",&c1,&c2) != EOF) {
-        i1 = c1 - 'A';
-        i2 = c2 - 'X';
-        score1 += ((i1+1) %3 == i2)*6 + (i1==i2)*3 + i2 + 1;
-        score2 += (i2 == 0)*((i1+2) % 3 + 1) + (i2 == 1)*(i1+4) + (i2 == 2)*((i1+1) % 3 + 7);
+        i1 = (c1 & 3) - 1;
+        i2 = c2 & 3;
+        score1 += ((c1 & 3) %3 == i2)*6 + (i1==i2)*3 + i2 + 1;
+        score2 += (((c1 & 3) + 1 + (c2 & 3)) % 3) + 1 + (3*(c2 & 3));
     }
     printf("score: %d\n",score1);
     printf("score: %d\n",score2);
